92bdb3deaf8488be627a0fc04d3741727cf3cc40
Renan Marinho Fernandes <renansycer@outlook.com>
2019-07-27 03:24:46 -0300

    conectando o banco via sequelize

--- config/pg_connection.js
+++ config/pg_connection.js
@@ -4,7 +4,29 @@
 
 'use strict';
 
+const Sequelize = require('sequelize');
 const urlConexao = `postgres://${process.env.BD_USUARIO}:${process.env.BD_SENHA}@${process.env.BD_HOST}:${process.env.BD_PORTA}/${process.env.BD_NOME}?application_name=${process.env.BD_NOME_APLICACAO}`;
 
+const db = new Sequelize(process.env.BD_NOME,process.env.BD_USUARIO, process.env.BD_SENHA,{
+    hots:process.env.BD_HOST,
+    dialect:'postgres'
+    },{
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
+
+db.authenticate()
+  .then(() => {
+     console.log('CONECTOU!!!!!');
+  })
+  .catch(err =>{
+      console.error('Deu erro :', err);
+  });
+
 module.exports = urlConexao;
+module.exports = db;
 
