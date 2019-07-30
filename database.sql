92909b750269b9a6e37911313d1512387cb50afa
Renan Marinho Fernandes <renansycer@outlook.com>
2019-07-30 19:16:59 -0300

    alterações no script de criação do banco

--- database.sql
+++ database.sql
@@ -1,19 +1,25 @@
-CREATE TABLE Alunos(
+CREATE DATABASE "desafio-lemobs"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+	
+	CREATE TABLE Alunos(
     aluno_id SERIAL UNIQUE PRIMARY KEY,
     nome TEXT NOT NULL,
     data_nascimento DATE NOT NULL,
     cpf TEXT UNIQUE NOT NULL,
-    nota NUMBER NOT NULL,
-    
-);
-
-
-CREATE TABLE Enderecos(
+    nota NUMERIC NOT NULL);
+	
+	CREATE TABLE Enderecos(
     endereco_id SERIAL UNIQUE PRIMARY KEY,
     rua TEXT NOT NULL,
+	numero INT,
     complemento TEXT ,
-    bairro VARCHAR(50) NOT NULL,
+    bairro TEXT NOT NULL,
     aluno_id INT NOT NULL,
-    (endereco_id),
-    FOREIGN KEY (aluno_id) REFERENCES Alunos (aluno_id)
-);
+    FOREIGN KEY (aluno_id) REFERENCES Alunos (aluno_id));
+	
